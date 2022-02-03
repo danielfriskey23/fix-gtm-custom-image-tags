1df1a4e96cfa8a0e2292a2727b5f2b6ad5f46530
Daniel Friskey <54420124+danielfriskey23@users.noreply.github.com>
2022-02-03 13:27:55 -0600

    Update fix.js

--- fix.js
+++ fix.js
@@ -1,3 +1,4 @@
+//REPLACE "XXXX" WITH A STRING FROM THE TAGS IMAGE SRC
 function checkTag() {
   if ( $('img[src*="XXXX"]') ) {
     $('img[src*="XXXX"]').css('width','1px').css('height','1px');
@@ -6,5 +7,3 @@ function checkTag() {
 }
   
 var myInterval = setInterval(checkTag, 1000);
-
-//REPLACE "XXXX" WITH A STRING FROM THE TAGS IMAGE SRC
